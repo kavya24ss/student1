78e68a069c20b4856cce9dcec3a42b55af753393
kavya24ss <sagikavyasri@gmail.com>
2025-10-17 23:00:51 +0530

    Update student.py

--- student.py
+++ student.py
@@ -2,3 +2,8 @@ principal = float(input("Enter the Principal amount: "))
 rate = float(input("Enter the Rate of Interest (in %): ")) 
 time = float(input("Enter the Time (in years): ")) 
 simple_interest = (principal * rate * time) / 100 
+amount = principal * (1 + rate / 100) ** time 
+compound_interest = amount - principal 
+print("\n--- Results ---") 
+print("Simple Interest =", simple_interest) 
+print("Compound Interest =", compound_interest) 
